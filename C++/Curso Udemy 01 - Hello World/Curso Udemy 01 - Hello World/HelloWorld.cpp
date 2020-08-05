666b91f05fa91e9bbcf36f1a8a6af92e6dd0434d
yhanchristian <yhan.christian@live.com>
2020-08-05 13:21:55 -0300

    Aula 01 e Tarefa 01 do curso C++ Udemy

---
+++ C++/Curso Udemy 01 - Hello World/Curso Udemy 01 - Hello World/HelloWorld.cpp
@@ -0,0 +1,13 @@
+/* Primeiro Exemplo Curso C++ Udemy
+   Hello World
+*/
+
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	system("PAUSE");
+	return 0;
+}
---
+++ C++/Curso Udemy 02 - Primeira Tarefa/Curso Udemy 02 - Primeira Tarefa/PrimeiraTarefa.cpp
@@ -0,0 +1,15 @@
+/* Primeiro Exemplo Curso C++ Udemy
+   Primeira Tarefa: Exibir Nome, Idade e 1 frase
+*/
+
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Yhan Christian" << std::endl;
+	std::cout << "26 anos" << std::endl;
+	std::cout << "Palmeiras nao tem mundial!" << std::endl;
+	system("PAUSE");
+	return 0;
+}

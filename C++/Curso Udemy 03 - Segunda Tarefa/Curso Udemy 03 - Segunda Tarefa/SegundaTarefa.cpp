5eb33fb68af6cdff0b657fe3bf33aeecf91d1407
yhanchristian <yhan.christian@live.com>
2020-08-05 13:45:40 -0300

    tarefa 02 - curso c++

---
+++ C++/Curso Udemy 03 - Segunda Tarefa/Curso Udemy 03 - Segunda Tarefa/SegundaTarefa.cpp
@@ -0,0 +1,16 @@
+/* Segunda atividade - Curso C++
+	Escrever na tela "EU VOU APRENDER C++" no cmd
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "EU" << std::endl;
+	std::cout << "VOU" << std::endl;
+	std::cout << "APRENDER\n";
+	std::cout << "C++" << std::endl;
+
+	system("PAUSE");
+	return 0;
+}

dd144e4a501cd9c93b0070d9a8ac48f6ed7032b4
Yhan Christian <yhan.christian@live.com>
2021-07-09 15:02:44 -0300

    Cap 03 - Curso DSA Análise Dados Python

--- Python/Cap03/Lab02/calculadora_v1.py
+++ Python/Cap03/Lab02/calculadora_v1.py
@@ -4,6 +4,47 @@
 # A solução será apresentada no próximo capítulo!
 # Assista o vídeo com a execução do programa!
 
+
 print("\n******************* Python Calculator *******************")
 
+def soma( num_1, num_2):
+    print("Primeiro número: " + str(num_1))
+    print("Segundo número: " + str(num_2))
+    print("Soma: ", int(num_1) + int(num_2))
+    
+
+def sub(num_1, num_2):
+    print("Primeiro número: " + str(num_1))
+    print("Segundo número: " + str(num_2))
+    print("Sub: ", int(num_1) - int(num_2))
+
+
+def mult(num_1, num_2):
+    print("Primeiro número: " + str(num_1))
+    print("Segundo número: " + str(num_2))
+    print("Mult: ", int(num_1) * int(num_2))
+
+def div(num_1, num_2):
+    print("Primeiro número: " + str(num_1))
+    print("Segundo número: " + str(num_2))
+    print("Div: ",int(num_1) / int(num_2))
+
+operacao = input('Digite a operação desejada (1 - soma 2 - sub 3 - mult 4 - div): ')
+valor_1 = input('Digite numero 1: ' )
+valor_2 = input('Digite numero 2: ')
+
+if(operacao == '1'):
+    soma(valor_1, valor_2)
+    
+elif(operacao == '2'):
+    sub(valor_1, valor_2)
+
+elif(operacao == '3'):
+    mult(valor_1, valor_2)    
+
+elif(operacao == '4'):
+    div(valor_1, valor_2) 
+
+else:
+    print ('Operação inválida!')
 

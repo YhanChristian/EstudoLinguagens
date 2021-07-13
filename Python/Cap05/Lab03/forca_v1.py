81a154613e119ca29737dd5e08499414fbd775fe
Yhan Christian <yhan.christian@live.com>
2021-07-13 10:38:07 -0300

    Game forca.py com Classe e utilização de métodos v1

--- Python/Cap05/Lab03/forca_v1.py
+++ Python/Cap05/Lab03/forca_v1.py
@@ -71,26 +71,106 @@ class Hangman:
 
 	# Método Construtor
 	def __init__(self, word):
+		self.failed_attemps = 0
+		self.word = word
+		self.game_progress = list('_' * len(self.word))
 		
 		
 	# Método para adivinhar a letra
 	def guess(self, letter):
-		
-		
-	# Método para verificar se o jogo terminou
-	def hangman_over(self):
-		
-		
-	# Método para verificar se o jogador venceu
-	def hangman_won(self):
-		
+		return [i for i, char in enumerate(self.word) if letter == char]
 
-	# Método para não mostrar a letra no board
-	def hide_word(self):
+	
+	#Método para verificar se entrada é válida (letra)
+	#:param input_: String, user input to be validated
+	def is_invalid_letter(self, input_):
+		return input_.isdigit() or (input_.isalpha() and len(input_) > 1)
+
+	#Método para atualizar game com letras 
+	def update_progress(self, letter, indexes):
+		for index in indexes:
+			self.game_progress[index] = letter
 		
-		
-	# Método para checar o status do game e imprimir o board na tela
+	#Método para obter entrada digitada pelo usuário	
+	def get_user_input(self):
+		user_input = input('\nDigite uma letra: ')
+		return user_input
+	
+	#Método para exibir palavras em branco
 	def print_game_status(self):
+		print('\n')
+		print('\n'.join(board[:self.failed_attemps]))
+		print('\n')
+		print(' '.join(self.game_progress))
+
+
+	#Método para exibir mensagem de vencedor
+	def hangman_won_game(self):
+		print("Parabéns, você ganhou!")
+		print ('A palavra eh: {0}'.format(self.word))
+		print("       ___________      ")
+		print("      '._==_==_=_.'     ")
+		print("      .-\\:      /-.    ")
+		print("     | (|:.     |) |    ")
+		print("      '-|:.     |-'     ")
+		print("        \\::.    /      ")
+		print("         '::. .'        ")
+		print("           ) (          ")
+		print("         _.' '._        ")
+		print("        '-------'       ")
+
+	#Método para exibir mensagem de derrota 
+
+	def hangman_lose_game(self):
+		print("Puxa, você foi enforcado!")
+		print('A palavra eh: {0}'.format(self.word))
+		print("    _______________         ")
+		print("   /               \       ")
+		print("  /                 \      ")
+		print("//                   \/\  ")
+		print("\|   XXXX     XXXX   | /   ")
+		print(" |   XXXX     XXXX   |/     ")
+		print(" |   XXX       XXX   |      ")
+		print(" |                   |      ")
+		print(" \__      XXX      __/     ")
+		print("   |\     XXX     /|       ")
+		print("   | |           | |        ")
+		print("   | I I I I I I I |        ")
+		print("   |  I I I I I I  |        ")
+		print("   \_             _/       ")
+		print("     \_         _/         ")
+		print("       \_______/           ")
+
+
+	
+	#Método com mecânica de jogo da forca
+	def play_game(self):
+
+		while self.failed_attemps < len(board):
+			self.print_game_status()
+			user_input = self.get_user_input()
+
+			#Valida a entrada do usuário com método is_invalid_letter
+			if self.is_invalid_letter(user_input):
+				print('\n A entrada nao eh uma letra valida!')
+				continue	
+			#Verifica se letra já não foi digitada anteriormente
+			if user_input in self.game_progress:
+				print('\n Voce ja digitou essa letra anteriormente!')
+				continue
+			#Verifica se usuario venceu o game 
+			if user_input in self.word:
+				indexes = self.guess(user_input)
+				self.update_progress(user_input, indexes)
+
+				#Verifica se não há letras para procurar na palavra
+				if self.game_progress.count('_') == 0:
+					self.hangman_won_game()
+					quit()
+			else:
+				self.failed_attemps += 1
+				
+		self.hangman_lose_game()
 		
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
@@ -106,20 +186,7 @@ def main():
 	# Objeto
 	game = Hangman(rand_word())
 
-	# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
-	
-
-	# Verifica o status do jogo
-	game.print_game_status()	
-
-	# De acordo com o status, imprime mensagem na tela para o usuário
-	if game.hangman_won():
-		print ('\nParabéns! Você venceu!!')
-	else:
-		print ('\nGame over! Você perdeu.')
-		print ('A palavra era ' + game.word)
-		
-	print ('\nFoi bom jogar com você! Agora vá estudar!\n')
+	game.play_game()	
 
 # Executa o programa		
 if __name__ == "__main__":

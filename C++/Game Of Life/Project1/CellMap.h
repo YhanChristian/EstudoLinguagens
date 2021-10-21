29326564bff7620e1ef113653d5a8b6cb11f7e23
Yhan Christian <yhan.christian@live.com>
2021-10-21 17:50:30 -0300

    Cap. Algoritmos ML

--- C++/Game Of Life/Project1/CellMap.h
+++ C++/Game Of Life/Project1/CellMap.h
@@ -1,1 +1,53 @@
+/**
+  ******************************************************************************
+  * @Company    : Yhan Christian Souza Silva
+  * @file       : Cellmap.h
+  * @author     : Yhan Christian Souza Silva
+  * @date       : 19/10/2021
+  * @brief      : Arquivo header implementação classe CellMap
+  ******************************************************************************
+*/
+
 #pragma once
+
+/* Includes ------------------------------------------------------------------*/
+
+#include <ctime>
+#include <iostream>
+#include "GameOfLife.h"
+
+/* Define --------------------------------------------------------------------*/
+
+#define OFF_COLOUR 0x00
+#define ON_COLOUR 0xFF
+
+
+// Estrutura da Células
+
+/*As células são armazenadas em caracteres de 8 bits, onde o 0º bit representa
+o estado da célula e o 1º ao 4º bit representam o número
+de vizinhos(até 8).O 5º ao 7º bits não são usados.
+Referência: http://www.jagregory.com/abrash-black-book/images/17-03.jpg
+*/
+
+//CellMap: armazena um array de células com seus respectivos estados
+
+class CellMap
+{
+public:
+	CellMap(unsigned int w, unsigned int h);
+	~CellMap();
+	void SetCell(unsigned int x, unsigned int y);
+	void ClearCell(unsigned int x, unsigned int y);
+	int CellState(int x, int y);
+	void NextGen();
+	void Init();
+private:
+	unsigned char* cells;
+	unsigned char* temp_cells;
+	unsigned int width;
+	unsigned int height;
+	int length_in_bytes;
+};
+
+/*****************************END OF FILE**************************************/
--- C++/Game Of Life/Project1/GameOfLife.h
+++ C++/Game Of Life/Project1/GameOfLife.h
@@ -1,7 +1,35 @@
+/**
+  ******************************************************************************
+  * @Company    : Yhan Christian Souza Silva
+  * @file       : GameOfLife.h
+  * @author     : Yhan Christian Souza Silva
+  * @date       : 19/10/2021
+  * @brief      : Arquivo header com defines e bibliotecas utilizadas
+  ******************************************************************************
+*/
+
+/* Define --------------------------------------------------------------------*/
+
 #define SDL_MAIN_HANDLED
+#define OFF_COLOUR 0x00
+#define ON_COLOUR 0xFF
+
+// Limit loop rate 
+#define LIMIT_RATE 1
+// Tick-rate in milliseconds (se LIMIT_RATE == 1)
+#define TICK_RATE 50
+
+/* Includes ------------------------------------------------------------------*/
 
 #include <SDL.h>
-#include <ctime>
-#include <iostream>
 #include <Windows.h>
+#include <locale.h>
 #include "CellMap.h"
+
+/* Private function prototypes -----------------------------------------------*/
+
+void DrawCell(unsigned int x, unsigned int y, unsigned int colour);
+
+
+
+/*****************************END OF FILE**************************************/

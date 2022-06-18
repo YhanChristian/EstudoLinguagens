a9511d4ded6f62dea70b883a842c7864f1970779
Yhan Christian <yhan.christian@live.com>
2022-06-18 12:01:42 -0300

    Estudo sintaxe blade php

--- PHP/app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ PHP/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -26,3 +26,34 @@
 @else
     <h3>Ainda não existem fornecedores cadastrados</h3>
 @endif
+
+{{-- Operador unless (funciona basicamente como uma negação --}}
+
+{{-- Lógica inversa seria como if(!(condicao))--}}
+@unless($new_fornecedores[0]['status'] == 'S')
+    <h3>Fornecedor Inativo</h3>
+@endunless
+
+{{-- Operador isset retorna true se a variável estiver definida --}}
+
+@isset($new_fornecedores)
+    Fornecedor: {{$new_fornecedores[0]['nome']}}
+    <br>
+    Status: {{$new_fornecedores[0]['status']}}
+    <br>
+    CNPJ: {{$new_fornecedores[0]['cnpj']}}
+    <br>
+@endisset
+<br>
+<br>
+@isset($new_fornecedores)
+    Fornecedor: {{$new_fornecedores[1]['nome']}}
+    <br>
+    Status: {{$new_fornecedores[1]['status']}}
+    <br>
+    {{-- Testa se existe antes imprimir, como n foi definido não entra neste bloco --}}
+    @isset($new_fornecedores[1]['cnpj'])
+        CNPJ: {{$new_fornecedores[1]['cnpj']}}
+    <br>
+    @endisset
+@endisset

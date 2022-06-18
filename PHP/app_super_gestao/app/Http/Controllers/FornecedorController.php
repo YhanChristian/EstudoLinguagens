717105bf9c3b15ae1c42fd8394b6737061f9d3d9
Yhan Christian <yhan.christian@live.com>
2022-06-18 12:52:24 -0300

    Estudo sintaxe blade php - Loops

--- PHP/app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ PHP/app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -9,9 +9,16 @@ class FornecedorController extends Controller
     public function index(){
         $fornecedores =['Fornecedor 1'];
         $new_fornecedores = [
-            0 =>['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj'=> '00.000.000/0000-00'],
-            1 =>['nome' => 'Fornecedor 2', 'status' => 'S',]
+            0 =>['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj'=> '00.000.000/0000-00', 'ddd' =>'11', 'tel'=> '0000-0000'],
+            1 =>['nome' => 'Fornecedor 2', 'status' => 'S','ddd' =>'13', 'tel'=> '1111-1111'],
+            2 =>['nome' => 'Fornecedor 3', 'status' => 'N', 'cnpj'=> '99.000.000/0001-00','ddd' =>'21', 'tel'=> '9999-9999'],
+            3 =>['nome' => 'Fornecedor 4', 'status' => 'S','ddd' =>'19', 'tel'=> '8888-8888']
         ];
+        //Operador Ternário aplicação
+        echo isset($new_fornecedores[0]['cnpj']) ? 'CNPJ Informado': 'CNPJ Não Informado';
+
+
         return view('app.fornecedor.index', compact('fornecedores'), compact('new_fornecedores'));
     }
+
 }
--- PHP/app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ PHP/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -56,4 +56,112 @@
         CNPJ: {{$new_fornecedores[1]['cnpj']}}
     <br>
     @endisset
+    <br>
+    {{-- Uso Default caso a variável n esteja definida @default --}}
+    Fornecedor: {{$new_fornecedores[3]['nome']}}
+    <br>
+    Status: {{$new_fornecedores[3]['status']}}
+    <br>
+    CNPJ: {{$new_fornecedores[3]['cnpj'] ?? 'Dado não prenchido'}}
+    <br>
+    DDD: {{$new_fornecedores[3]['ddd'] ?? 'Dado não prenchido'}}
+    <br>
+    Tel: {{$new_fornecedores[3]['tel'] ?? 'Dado não prenchido'}}
+    <br>
+    {{-- Switch Case --}}
+    @switch($new_fornecedores[3]['ddd'])
+        @case('11')
+            São Paulo - SP
+        @break
+        @case('13')
+            Santos - SP
+        @break
+        @case('19')
+        Campinas - SP
+        @break
+        @case('21')
+        Rio de Janeiro - RJ
+        @break
+        @default
+            Estado não identificado
+        @break
+    @endswitch
 @endisset
+
+<br>
+<h3>uso do laço for</h3>
+
+@isset($new_fornecedores)
+    @for($i = 0; isset($new_fornecedores[$i]); $i++)
+        Fornecedor: {{$new_fornecedores[$i]['nome']}}
+        <br>
+        Status: {{$new_fornecedores[$i]['status']}}
+        <br>
+        CNPJ: {{$new_fornecedores[$i]['cnpj'] ?? 'Dado não prenchido'}}
+        <br>
+        DDD: {{$new_fornecedores[$i]['ddd'] ?? 'Dado não prenchido'}}
+        <br>
+        Tel: {{$new_fornecedores[$i]['tel'] ?? 'Dado não prenchido'}}
+        <br>
+    @endfor
+@endisset
+
+<br>
+<h3>uso do while</h3>
+@isset($new_fornecedores)
+    @php $i = 0 @endphp
+    @while(isset($new_fornecedores[$i]))
+        Fornecedor: {{$new_fornecedores[$i]['nome']}}
+        <br>
+        Status: {{$new_fornecedores[$i]['status']}}
+        <br>
+        CNPJ: {{$new_fornecedores[$i]['cnpj'] ?? 'Dado não prenchido'}}
+        <br>
+        DDD: {{$new_fornecedores[$i]['ddd'] ?? 'Dado não prenchido'}}
+        <br>
+        Tel: {{$new_fornecedores[$i]['tel'] ?? 'Dado não prenchido'}}
+        <br>
+        @php $i++ @endphp
+    @endwhile
+@endisset
+
+<br>
+<h3>uso foreach</h3>
+@isset($new_fornecedores)
+   @foreach($new_fornecedores as $indice => $new_fornecedores)
+        Iteração Atual: {{$loop->iteration}}
+        <br>
+        Total Count: {{$loop->count}}
+        @if($loop->first)
+            <br>
+            Primeira iteração do loop
+        @endif
+        @if($loop->last)
+            <br>
+            Ultima iteração do loop
+        @endif
+        <br>
+        Fornecedor: {{$new_fornecedores['nome']}}
+        <br>
+        Status: {{$new_fornecedores['status']}}
+        <br>
+        CNPJ: {{$new_fornecedores['cnpj'] ?? 'Dado não prenchido'}}
+        <br>
+        DDD: {{$new_fornecedores['ddd'] ?? 'Dado não prenchido'}}
+        <br>
+        Tel: {{$new_fornecedores['tel'] ?? 'Dado não prenchido'}}
+        <br>
+    @endforeach
+@endisset
+
+{{-- Ao colocar @ na frente '{{}}' não é interpretado, simplesmente conteúdo é impresso --}}
+<h5>@{{$bloco não interpretado}}</h5>
+
+{{-- Verifica se variável esta ou não vazia --}}
+@empty($new_fornecedores[2]['cnpj'])
+    <h3>Vazio</h3>
+@endempty
+
+
+
+

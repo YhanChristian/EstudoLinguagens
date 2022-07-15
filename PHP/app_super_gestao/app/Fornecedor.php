871cf427a49542ecec1e7802c3d72a92c86da716
Yhan Christian <yhan.christian@live.com>
2022-07-14 22:38:41 -0300

    Trabalhando com Eloquent ORM

--- PHP/app_super_gestao/app/Fornecedor.php
+++ PHP/app_super_gestao/app/Fornecedor.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    //Sobrepõe a nomeação do Eloquent da tabela
+    // por padrão o eloquent usa o nome minusculo da classe e acrescenta o 's' como plural
+    // Para sobrepor dentro da classe extendida no model utilizamos o atributo protected table e renomeamos a tabela.
+
+    protected $table = 'fornecedores';
+    //Permite a inserção dos atributos nome, site, uf e email sem necessidade de instanciar o objeto
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }

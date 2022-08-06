3c48a24ae496233b1cedb52dc4127964d2b5caab
Yhan Christian <yhan.christian@live.com>
2022-08-06 15:14:49 -0300

    Estudo eloquent ORM com Laravel.

--- PHP/app_super_gestao/app/SiteContato.php
+++ PHP/app_super_gestao/app/SiteContato.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SiteContato extends Model
 {
     //
+    protected $table = 'site_contatos';
+    //Permite a inserção dos atributos nome, site, uf e email sem necessidade de instanciar o objeto
+    protected $fillable = ['nome', 'email', 'telefone'];
 }

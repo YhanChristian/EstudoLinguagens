39bfe927dd6ddfad5e2ce182c1fa00bdc6ac8ea8
Yhan Christian <yhan.christian@live.com>
2022-08-17 20:26:57 -0300

    Formulários - Laravel

--- PHP/app_super_gestao/app/SiteContato.php
+++ PHP/app_super_gestao/app/SiteContato.php
@@ -6,8 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
-    //
-    protected $table = 'site_contatos';
-    //Permite a inserção dos atributos nome, site, uf e email sem necessidade de instanciar o objeto
-    protected $fillable = ['nome', 'email', 'telefone'];
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
 }

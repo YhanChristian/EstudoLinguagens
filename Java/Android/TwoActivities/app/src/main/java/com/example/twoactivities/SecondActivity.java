5d047ce76681134039d58c57b22840f7ca67b702
Yhan Christian <yhan.christian@live.com>
2023-02-01 15:36:15 -0300

    Implementação Two Activities (Exemplo)

---
+++ Java/Android/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -0,0 +1,53 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * SecondActivity defines the second activity in the app. It is
+ * launched from an intent with a message, and sends an intent
+ * back with a second message.
+ */
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.twoactivities.extra.REPLY";
+    private EditText mReply;
+
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mReply = findViewById(R.id.editText_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+    /**
+     * Handles the onClick for the "Reply" button. Gets the message from the
+     * second EditText, creates an intent, and returns that message back to
+     * the main activity.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        // Create a new intent for the reply, add the reply message to it
+        // as an extra, set the intent result, and close the activity.
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK,replyIntent);
+        finish();
+    }
+}

ef5810a1b0d5b239ca714dd66a1017d8e48362e4
Yhan Christian <yhan.christian@live.com>
2024-01-12 08:28:01 -0300

    App FitnessTrakcer - Em andamento (UI concluída).

---
+++ Kotlin/FitnessTracker/app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -0,0 +1,77 @@
+package co.tiagoaguiar.fitnesstracker
+
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
+
+class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editHeight: EditText
+    private lateinit var editWeight: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+
+        editHeight = findViewById(R.id.edit_imc_height)
+        editWeight = findViewById(R.id.edit_imc_weight)
+
+        val btnCalcImc: Button = findViewById(R.id.btn_imc)
+
+        btnCalcImc.setOnClickListener {
+            if (!isValidFields()) {
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val weight = editWeight.text.toString().toInt()
+            val height = editHeight.text.toString().toInt()
+            val imc = calcImc(weight, height)
+            val result = showResult(imc)
+
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.imc_response, imc))
+                .setMessage(getString(result))
+                .setPositiveButton(android.R.string.ok) { _: DialogInterface, _: Int ->
+                    Log.d("CLICK", "save")
+                }
+                .create()
+                .show()
+
+            /* Hide keyboard */
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
+    }
+    private fun isValidFields(): Boolean {
+        return (editWeight.text.toString().isNotEmpty()
+                && editHeight.text.toString().isNotEmpty()
+                && !editWeight.text.toString().startsWith("0")
+                && !editHeight.text.toString().startsWith("0"))
+    }
+
+    private fun calcImc(weight: Int, height: Int): Double {
+        return weight / ((height / 100.0) * (height / 100.0))
+    }
+
+    @StringRes
+    private fun showResult(result: Double): Int {
+        return when {
+            result < 15.0 -> R.string.imc_severely_low_weight
+            result < 16.0 -> R.string.imc_very_low_weight
+            result < 18.5 -> R.string.imc_low_weight
+            result < 25.0 -> R.string.normal
+            result < 30.0 -> R.string.imc_high_weight
+            result < 35.0 -> R.string.imc_so_high_weight
+            result < 40.0 -> R.string.imc_severely_high_weight
+            else -> R.string.imc_extreme_weight
+        }
+    }
+}
